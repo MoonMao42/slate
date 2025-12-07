b8851b3808746547a50171b4370cf84364f36fcc
MoonMao <mky369258@gmail.com>
2025-12-07 10:36:52 +0800

    cli: route status command to status_panel::render()

--- src/cli/status.rs
+++ src/cli/status.rs
@@ -1,9 +1,6 @@
-use crate::brand::language::Language;
 use crate::error::Result;
 
 /// Handle `slate status` command
 pub fn handle(_args: &[&str]) -> Result<()> {
-    // will implement full status display
-    println!("{}", Language::STATUS_PENDING);
-    Ok(())
+    super::status_panel::render()
 }
